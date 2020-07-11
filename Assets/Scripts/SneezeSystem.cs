fa62908ab25c7bb2d4e948b8d0fbf07b7eb5f91a
Jeremy Boulet <jeremyboulet85@gmail.com>
2020-07-11 02:31:56 -0400

    Sneeze system

---
+++ Assets/Scripts/SneezeSystem.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class SneezeSystem : MonoBehaviour
+{
+
+    public GaugeBarController sneezeBar;
+    int sneezeLevel;
+    const int maxSneezeLevel = 100;
+    const float delayAmount = 0.1f; // seconds
+
+    protected float timer;
+
+    void Start()
+    {
+        timer = 0;
+        sneezeLevel = 0;
+        sneezeBar.SetMax(maxSneezeLevel);
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= delayAmount)
+        {
+            timer = 0f;
+            IncreaseSneezeLevel();
+        }
+    }
+
+    void IncreaseSneezeLevel()
+    {
+        sneezeLevel += 1;
+        sneezeBar.IncreaseValue(1);
+
+        if (sneezeLevel >= maxSneezeLevel)
+            Sneeze();
+    }
+
+    void Sneeze()
+    {
+        sneezeLevel = 0;
+        sneezeBar.SetValue(0);
+        Debug.Log("Sneeze!!");
+        // do more things
+    }
+}
