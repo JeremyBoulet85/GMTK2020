eb203aa2697e26b6de85f979a3f3b4d1ba84f89a
Jeremy Boulet <jeremyboulet85@gmail.com>
2020-07-11 19:06:42 -0400

    Fart System

--- Assets/Scripts/FartSystem.cs
+++ Assets/Scripts/FartSystem.cs
@@ -2,15 +2,47 @@
 
 public class FartSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GaugeBarController fartBar;
+
+    SoundManager soundManager;
+    float fartLevel;
+    const int maxFartLevel = 100;
+    const float fillingDelayAmount = 0.25f; // seconds
+
+    protected float timer;
+
     void Start()
     {
-        
+        timer = 0;
+        fartLevel = 0f;
+        fartBar.SetMax(maxFartLevel);
+        soundManager = GetComponent<SoundManager>();
     }
 
-    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+    }
+
     void Update()
     {
-        
+        if (timer >= fillingDelayAmount && !fartBar.isDraining)
+        {
+            timer = 0f;
+            IncreaseSneezeLevel();
+        }
+    }
+
+    void IncreaseSneezeLevel()
+    {
+        fartLevel += 0.5f;
+        fartBar.IncreaseValue(0.5f);
+
+        if (fartLevel >= maxFartLevel)
+        {
+            fartLevel = 0;
+            soundManager.PlaySound(SoundType.Sneeze, 0.9f);
+            fartBar.isDraining = true;
+        }
     }
 }
