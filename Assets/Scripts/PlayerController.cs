f48539bf7b364e8bd76240e4c266dd249af7de05
fx46 <31199346+fx46@users.noreply.github.com>
2020-07-10 22:59:23 -0400

    New player prefab with basic player controller script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 3.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = new Vector3();
+
+        velocity.y += Input.GetKey(KeyCode.W) ? 1.0f : 0.0f;
+        velocity.x -= Input.GetKey(KeyCode.A) ? 1.0f : 0.0f;
+        velocity.y -= Input.GetKey(KeyCode.S) ? 1.0f : 0.0f;
+        velocity.x += Input.GetKey(KeyCode.D) ? 1.0f : 0.0f;
+
+        transform.Translate(velocity.normalized * speed * Time.deltaTime);
+    }
+}
