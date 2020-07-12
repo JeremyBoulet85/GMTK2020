f6131413dcc083be2f6127ac4b030948f8a94869
Duc-Thien Nguyen <duc-thien@sympatico.ca>
2020-07-12 04:13:17 -0400

    Revert "merge"
    This reverts commit a0faaa20c8cb3aa84cb47cca87b07869f7811348.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
+        Toggle toggle = GameObject.FindGameObjectWithTag("mode").GetComponent<Toggle>();
+        GameManager.instance.IsHardMode = !toggle.isOn;
         SceneManager.LoadScene("MainScene");
     }
 
